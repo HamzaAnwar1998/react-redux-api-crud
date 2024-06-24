88fa3200daebb9a0c948935c93fd6c887e2f0ba2
Hamza <anwarhamza919@gmail.com>
2024-06-24 06:45:23 -0400

    fix loading issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "./store/slices/UsersSlice";
 import Users from "./components/Users";
-import { MetroSpinner } from "react-spinners-kit";
 import { MDBBtn } from "mdb-react-ui-kit";
 import { Toaster } from "react-hot-toast";
 import DeleteUserModal from "./components/DeleteUserModal";
@@ -12,17 +8,6 @@ import EditUserModal from "./components/EditUserModal";
 import AddUserModal from "./components/AddUserModal";
 
 function App() {
-  // redux state
-  const { loading } = useSelector((state) => state.users);
-
-  // dispatch
-  const dispatch = useDispatch();
-
-  // fetch users
-  useEffect(() => {
-    dispatch(fetchUsers());
-  }, []);
-
   // delete modal
   const [deleteUserModal, setDeleteUserModal] = useState(false);
   const [deleteUserId, setDeleteUserId] = useState(null);
@@ -46,29 +31,23 @@ function App() {
 
   return (
     <>
-      {loading ? (
-        <div className="loading-screen">
-          <MetroSpinner loading={loading} size={30} color="#007c16" />
+      <div className="wrapper">
+        <Toaster />
+        {/* nav */}
+        <div className="nav">
+          <h5>React Redux Clientside API CRUD</h5>
+          <MDBBtn color="success" onClick={() => setAddUserModal(true)}>
+            Add User
+          </MDBBtn>
         </div>
-      ) : (
-        <div className="wrapper">
-          <Toaster />
-          {/* nav */}
-          <div className="nav">
-            <h5>React Redux Clientside API CRUD</h5>
-            <MDBBtn color="success" onClick={() => setAddUserModal(true)}>
-              Add User
-            </MDBBtn>
-          </div>
-          {/* user table */}
-          <div className="users-table-container">
-            <Users
-              handleDeleteModal={handleDeleteModal}
-              handleEditModal={handleEditModal}
-            />
-          </div>
+        {/* user table */}
+        <div className="users-table-container">
+          <Users
+            handleDeleteModal={handleDeleteModal}
+            handleEditModal={handleEditModal}
+          />
         </div>
-      )}
+      </div>
       {/* delete modal */}
       {deleteUserModal && (
         <DeleteUserModal
